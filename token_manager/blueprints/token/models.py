a6902b3c124e58c4fa606205f9fe3a532b7f922e
novak.mateusz94@gmail.com <BigData@123>
2020-11-26 21:03:46 +0100

    Created first models for the database.

---
+++ token_manager/blueprints/token/models.py
@@ -0,0 +1,29 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from token_manager.app.extensions import db
+
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'Role: {self.name}'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), unique=True, nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
+
+    def __repr__(self):
+        return f'User: {self.username}'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
