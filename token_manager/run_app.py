39dd8829f0169062a42d9a8238e31659c51ba15f
novak.mateusz94@gmail.com <BigData@123>
2020-12-04 22:38:49 +0100

    Added ssl certificates

--- token_manager/run_app.py
+++ token_manager/run_app.py
@@ -1,6 +1,8 @@
+import os
 from token_manager.app.app_factory import create_app
 
 
 if __name__ == '__main__':
     token_app = create_app()
-    token_app.run(host='0.0.0.0', port=8001)
+    ssl_context = (os.path.join('certs', 'cert.pem'), os.path.join('certs', 'key.pem'))
+    token_app.run(host='0.0.0.0', port=8001, ssl_context=ssl_context)
